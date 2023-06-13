48d3be180ce8e1bfc5931ea62dbac564f51db22d
Tinsae Yared <tinsae.yb@gmail.com>
2023-06-12 20:05:16 -0500

    project created and db connected

---
+++ src/test/java/com/example/minipropertymanagement/MiniPropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minipropertymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPropertyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
